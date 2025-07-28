8a88dbf65e93dee4e621f487b16136d1e8aff04c
Sean Murphy <sean@iamseanmurphy.com>
2025-07-28 12:20:49 +0000

    16.2

--- avril/functions.php
+++ avril/functions.php
@@ -5,7 +5,7 @@ function avril_setup() {
 /**
  * Define Theme Version
  */
-define( 'AVRIL_THEME_VERSION', '16.1' );
+define( 'AVRIL_THEME_VERSION', '16.2' );
 
 // Root path/URI.
 define( 'AVRIL_PARENT_DIR', get_template_directory() );
